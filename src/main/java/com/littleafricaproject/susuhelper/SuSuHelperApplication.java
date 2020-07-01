4baf080428f51649a8d8879a160a8fee7ec7b92a
Ulric Todman <rick.sinclair.nyc@gmail.com>
2020-07-01 09:50:11 -0400

    initial commit

---
+++ src/main/java/com/littleafricaproject/susuhelper/SuSuHelperApplication.java
@@ -0,0 +1,13 @@
+package com.littleafricaproject.susuhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuSuHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuSuHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/littleafricaproject/susuhelper/SuSuHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littleafricaproject.susuhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuSuHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
